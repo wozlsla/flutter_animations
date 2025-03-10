e0a0d8bf1bd421705d85d51b346b894369fc40dc
jimin <jiminseo0307@gmail.com>
2025-03-10 22:49:54 +0900

    initial commit

---
+++ lib/screens/menu_screen.dart
@@ -0,0 +1,29 @@
+import 'package:animations/screens/implicit_animations_screen.dart';
+import 'package:flutter/material.dart';
+
+class MenuScreen extends StatelessWidget {
+  const MenuScreen({super.key});
+
+  void _goToPage(BuildContext context, Widget screen) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => screen));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Flutter Animations")),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                _goToPage(context, ImplicitAnimationsScreen());
+              },
+              child: Text("Implicit Animations"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
