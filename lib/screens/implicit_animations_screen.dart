a8dd96defabebdb2522d86a3054ed418cc95b1c5
jimin <jiminseo0307@gmail.com>
2025-03-11 10:59:46 +0900

    feat: implicit animations

--- lib/screens/implicit_animations_screen.dart
+++ lib/screens/implicit_animations_screen.dart
@@ -9,8 +9,45 @@ class ImplicitAnimationsScreen extends StatefulWidget {
 }
 
 class _ImplicitAnimationsScreenState extends State<ImplicitAnimationsScreen> {
+  bool _visible = true;
+
+  void _trigger() {
+    setState(() {
+      _visible = !_visible;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text("Implicit Animations")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TweenAnimationBuilder(
+              tween: ColorTween(
+                begin: Colors.amber.shade400,
+                end: Colors.blue.shade400,
+              ),
+              curve: Curves.bounceOut,
+              duration: Duration(seconds: 5),
+              builder: (context, value, child) {
+                return Opacity(
+                  opacity: _visible ? 1.0 : 0.6, // _trigger
+                  child: Image.network(
+                    "https://kili.aspix.it/Dash.png",
+                    color: value,
+                    colorBlendMode: BlendMode.colorBurn,
+                  ),
+                );
+              },
+            ),
+            SizedBox(height: 50),
+            ElevatedButton(onPressed: _trigger, child: Text("Start")),
+          ],
+        ),
+      ),
+    );
   }
 }
