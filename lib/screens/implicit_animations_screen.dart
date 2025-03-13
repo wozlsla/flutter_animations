f090e19329d443c0a5c8f2e9d43208f78df4f48d
jimin <jiminseo0307@gmail.com>
2025-03-13 17:02:57 +0900

    feat: explicit widgets with Tweens and A controller. (without AnimationBuilder)

--- lib/screens/implicit_animations_screen.dart
+++ lib/screens/implicit_animations_screen.dart
@@ -19,30 +19,43 @@ class _ImplicitAnimationsScreenState extends State<ImplicitAnimationsScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(title: Text("Implicit Animations")),
+      appBar: AppBar(title: const Text('Implict Animations')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TweenAnimationBuilder(
-              tween: ColorTween(
-                begin: Colors.amber.shade400,
-                end: Colors.blue.shade400,
+            AnimatedContainer(
+              curve: Curves.elasticOut,
+              duration: const Duration(seconds: 2),
+              width: size.width * 0.8,
+              height: size.width * 0.8,
+              transform: Matrix4.rotationZ(_visible ? 1 : 0),
+              transformAlignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: _visible ? Colors.red : Colors.amber,
+                borderRadius: BorderRadius.circular(_visible ? 100 : 0),
               ),
-              curve: Curves.bounceOut,
-              duration: Duration(seconds: 5),
-              builder: (context, value, child) {
-                return Opacity(
-                  opacity: _visible ? 1.0 : 0.6, // _trigger
-                  child: Image.network(
-                    "https://kili.aspix.it/Dash.png",
-                    color: value,
-                    colorBlendMode: BlendMode.colorBurn,
-                  ),
-                );
-              },
             ),
+            // TweenAnimationBuilder(
+            //   tween: ColorTween(
+            //     begin: Colors.amber.shade400,
+            //     end: Colors.blue.shade400,
+            //   ),
+            //   curve: Curves.bounceOut,
+            //   duration: Duration(seconds: 5),
+            //   builder: (context, value, child) {
+            //     return Opacity(
+            //       opacity: _visible ? 1.0 : 0.6, // _trigger
+            //       child: Image.network(
+            //         "https://kili.aspix.it/Dash.png",
+            //         color: value,
+            //         colorBlendMode: BlendMode.colorBurn,
+            //       ),
+            //     );
+            //   },
+            // ),
             SizedBox(height: 50),
             ElevatedButton(onPressed: _trigger, child: Text("Start")),
           ],
