7a1dc057ca50d93408d01a4063fc6b620afddad9
jimin <jiminseo0307@gmail.com>
2025-03-21 00:26:40 +0900

    feat: add interval with CurvedAnimation for staggered animation
    clip Foreground Scaffold (music player) - BackdropFilter, marquee

--- lib/screens/music_player_detail_screen.dart
+++ lib/screens/music_player_detail_screen.dart
@@ -40,6 +40,28 @@ class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen>
     duration: Duration(seconds: 5),
   );
 
+  final Curve _menuCurve = Curves.easeInOut;
+
+  late final Animation<double> _screenScale = Tween(
+    begin: 1.0,
+    end: 0.7,
+  ).animate(
+    CurvedAnimation(
+      parent: _menuController,
+      curve: Interval(0.0, 0.5, curve: _menuCurve),
+    ),
+  );
+
+  late final Animation<Offset> _screenOffset = Tween(
+    begin: Offset.zero,
+    end: Offset(0.5, 0),
+  ).animate(
+    CurvedAnimation(
+      parent: _menuController,
+      curve: Interval(0.5, 1.0, curve: _menuCurve),
+    ),
+  );
+
   String _timeFormatter(double value) {
     // value(0.0~1.0) -> 1m(60,000ms)
     final duration = Duration(milliseconds: (value * 60000).toInt());
@@ -98,7 +120,7 @@ class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen>
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        // back scaffold
+        // Background Scaffold
         Scaffold(
           backgroundColor: Colors.black,
           appBar: AppBar(
@@ -147,174 +169,194 @@ class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen>
           ),
         ),
 
-        // music player scaffold
-        Scaffold(
-          body: Stack(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/covers/${widget.index}.jpg"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 80, sigmaY: 80),
-                  child: Container(color: Colors.black.withValues(alpha: 0.65)),
-                ),
-              ),
-              Column(
-                children: [
-                  SizedBox(height: 70.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconButton(
-                        onPressed: () => Navigator.pop(context),
-                        icon: Icon(Icons.arrow_back_ios_new_rounded),
-                      ),
-                      IconButton(onPressed: _openMenu, icon: Icon(Icons.menu)),
-                    ],
-                  ),
-                  SizedBox(height: 50.0),
-                  Align(
-                    alignment: Alignment.center,
-                    child: Hero(
-                      tag: "${widget.index}",
-                      child: Container(
-                        width: 350.0,
-                        height: 350.0,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(14.0),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withValues(alpha: 0.5),
-                              blurRadius: 12,
-                              spreadRadius: 2,
-                              offset: Offset(0, 8),
-                            ),
-                          ],
-                          image: DecorationImage(
-                            image: AssetImage(
-                              "assets/covers/${widget.index}.jpg",
-                            ),
-                            fit: BoxFit.cover,
+        // Foreground Scaffold (music player) : Clip - BackdropFilter
+        SlideTransition(
+          position: _screenOffset,
+          child: ScaleTransition(
+            scale: _screenScale,
+            child: ClipRRect(
+              child: Scaffold(
+                body: Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(
+                            "assets/covers/${widget.index}.jpg",
                           ),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 80, sigmaY: 80),
+                        child: Container(
+                          color: Colors.black.withValues(alpha: 0.65),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 50.0),
-                  AnimatedBuilder(
-                    animation: _progressController,
-                    builder:
-                        (context, child) => Column(
+                    Column(
+                      children: [
+                        SizedBox(height: 70.0),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            CustomPaint(
-                              size: Size(barWidth, 7),
-                              painter: ProgressBar(
-                                progressValue: _progressController.value,
+                            IconButton(
+                              onPressed: () => Navigator.pop(context),
+                              icon: Icon(Icons.arrow_back_ios_new_rounded),
+                            ),
+                            IconButton(
+                              onPressed: _openMenu,
+                              icon: Icon(Icons.menu),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 50.0),
+                        Align(
+                          alignment: Alignment.center,
+                          child: Hero(
+                            tag: "${widget.index}",
+                            child: Container(
+                              width: 350.0,
+                              height: 350.0,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(14.0),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withValues(alpha: 0.5),
+                                    blurRadius: 12,
+                                    spreadRadius: 2,
+                                    offset: Offset(0, 8),
+                                  ),
+                                ],
+                                image: DecorationImage(
+                                  image: AssetImage(
+                                    "assets/covers/${widget.index}.jpg",
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
                               ),
                             ),
-                            const SizedBox(height: 10),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 40,
-                              ),
-                              child: Row(
+                          ),
+                        ),
+                        SizedBox(height: 50.0),
+                        AnimatedBuilder(
+                          animation: _progressController,
+                          builder:
+                              (context, child) => Column(
                                 children: [
-                                  Text(
-                                    _timeFormatter(_progressController.value),
-                                    style: TextStyle(
-                                      fontSize: 12,
-                                      color: Colors.grey,
-                                      fontWeight: FontWeight.w600,
+                                  CustomPaint(
+                                    size: Size(barWidth, 7),
+                                    painter: ProgressBar(
+                                      progressValue: _progressController.value,
                                     ),
                                   ),
-                                  Spacer(),
-                                  Text(
-                                    _timeFormatter(
-                                      1 - _progressController.value,
+                                  const SizedBox(height: 10),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                      horizontal: 40,
                                     ),
-                                    style: TextStyle(
-                                      fontSize: 12,
-                                      color: Colors.grey,
-                                      fontWeight: FontWeight.w600,
+                                    child: Row(
+                                      children: [
+                                        Text(
+                                          _timeFormatter(
+                                            _progressController.value,
+                                          ),
+                                          style: TextStyle(
+                                            fontSize: 12,
+                                            color: Colors.grey,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                        ),
+                                        Spacer(),
+                                        Text(
+                                          _timeFormatter(
+                                            1 - _progressController.value,
+                                          ),
+                                          style: TextStyle(
+                                            fontSize: 12,
+                                            color: Colors.grey,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                        ),
+                                      ],
                                     ),
                                   ),
                                 ],
                               ),
+                        ),
+                        const SizedBox(height: 20),
+                        const Text(
+                          "I DO ME",
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        SlideTransition(
+                          position: _marqueeTween,
+                          child: const Text(
+                            "KiiiKiii - The 1st EP [UNCUT GEM] Let's laugh out loud in this world with five kids!",
+                            maxLines: 1,
+                            overflow: TextOverflow.visible,
+                            softWrap: false,
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                        const SizedBox(height: 30.0),
+                        GestureDetector(
+                          onTap: _onPlayTap,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              AnimatedIcon(
+                                icon: AnimatedIcons.pause_play,
+                                progress: _playController,
+                                size: 60.0,
+                              ),
+                              // LottieBuilder.asset(
+                              //   "assets/icons/lottie_play.json",
+                              //   controller: _playController,
+                              //   onLoaded: (composition) {
+                              //     _playController.duration = composition.duration;
+                              //   },
+                              //   width: 150,
+                              //   height: 150,
+                              // ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 30.0),
+                        GestureDetector(
+                          onHorizontalDragUpdate: _onVolumeDragUpdate,
+                          onHorizontalDragStart: (_) => _toggleDragging(),
+                          onHorizontalDragEnd: (_) => _toggleDragging(),
+                          child: AnimatedScale(
+                            scale: _dragging ? 1.1 : 1,
+                            duration: Duration(milliseconds: 500),
+                            curve: Curves.bounceOut,
+                            child: Container(
+                              clipBehavior: Clip.hardEdge,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(4.0),
+                              ),
+                              child: ValueListenableBuilder(
+                                valueListenable: _volume,
+                                builder:
+                                    (context, value, child) => CustomPaint(
+                                      size: Size(barWidth, 16),
+                                      painter: VolumePaint(volume: value),
+                                    ),
+                              ),
                             ),
-                          ],
+                          ),
                         ),
-                  ),
-                  const SizedBox(height: 20),
-                  const Text(
-                    "I DO ME",
-                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
-                  ),
-                  const SizedBox(height: 5),
-                  SlideTransition(
-                    position: _marqueeTween,
-                    child: const Text(
-                      "KiiiKiii - The 1st EP [UNCUT GEM] Let's laugh out loud in this world with five kids!",
-                      maxLines: 1,
-                      overflow: TextOverflow.visible,
-                      softWrap: false,
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                  const SizedBox(height: 30.0),
-                  GestureDetector(
-                    onTap: _onPlayTap,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        AnimatedIcon(
-                          icon: AnimatedIcons.pause_play,
-                          progress: _playController,
-                          size: 60.0,
-                        ),
-                        // LottieBuilder.asset(
-                        //   "assets/icons/lottie_play.json",
-                        //   controller: _playController,
-                        //   onLoaded: (composition) {
-                        //     _playController.duration = composition.duration;
-                        //   },
-                        //   width: 150,
-                        //   height: 150,
-                        // ),
                       ],
                     ),
-                  ),
-                  const SizedBox(height: 30.0),
-                  GestureDetector(
-                    onHorizontalDragUpdate: _onVolumeDragUpdate,
-                    onHorizontalDragStart: (_) => _toggleDragging(),
-                    onHorizontalDragEnd: (_) => _toggleDragging(),
-                    child: AnimatedScale(
-                      scale: _dragging ? 1.1 : 1,
-                      duration: Duration(milliseconds: 500),
-                      curve: Curves.bounceOut,
-                      child: Container(
-                        clipBehavior: Clip.hardEdge,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(4.0),
-                        ),
-                        child: ValueListenableBuilder(
-                          valueListenable: _volume,
-                          builder:
-                              (context, value, child) => CustomPaint(
-                                size: Size(barWidth, 16),
-                                painter: VolumePaint(volume: value),
-                              ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ],
+            ),
           ),
         ),
       ],
