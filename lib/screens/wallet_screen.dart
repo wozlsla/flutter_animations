f6c0ffab063448de133935364fddcede5a1382c4
jimin <jiminseo0307@gmail.com>
2025-03-21 23:30:56 +0900

    feat: add card detail screen and animations with Hero, Material widget

--- lib/screens/wallet_screen.dart
+++ lib/screens/wallet_screen.dart
@@ -8,6 +8,8 @@ class WalletScreen extends StatefulWidget {
   State<WalletScreen> createState() => _WalletScreenState();
 }
 
+List<Color> colors = [Color(0xff4a4e69), Color(0xff008080), Color(0xff2a6f97)];
+
 class _WalletScreenState extends State<WalletScreen> {
   bool _isExpanded = false;
 
@@ -34,26 +36,17 @@ class _WalletScreenState extends State<WalletScreen> {
           onTap: _onExpanded,
           child: Column(
             children: [
-                  CreditCard(
-                        bgColor: Color(0xff4a4e69),
-                        isExpanded: _isExpanded,
-                      )
-                      .animate(target: _isExpanded ? 0 : 1, delay: 1.5.seconds)
-                      .flipV(end: 0.1),
-                  CreditCard(
-                        bgColor: Color(0xff008080),
-                        isExpanded: _isExpanded,
-                      )
-                      .animate(target: _isExpanded ? 0 : 1, delay: 1.5.seconds)
-                      .flipV(end: 0.1)
-                      .slideY(end: -0.8),
-                  CreditCard(
-                        bgColor: Color(0xff2a6f97),
-                        isExpanded: _isExpanded,
-                      )
-                      .animate(target: _isExpanded ? 0 : 1, delay: 1.5.seconds)
-                      .flipV(end: 0.1)
-                      .slideY(end: -0.8 * 2),
+                  for (var index in [0, 1, 2])
+                    Hero(
+                      tag: "$index",
+                      child: CreditCard(index: index, isExpanded: _isExpanded)
+                          .animate(
+                            target: _isExpanded ? 0 : 1,
+                            delay: 1.5.seconds,
+                          )
+                          .flipV(end: 0.1)
+                          .slideY(end: -0.8 * index),
+                    ),
                 ]
                 .animate(interval: 500.ms)
                 .fade(begin: 0)
@@ -65,83 +58,121 @@ class _WalletScreenState extends State<WalletScreen> {
   }
 }
 
-void _onTap() {}
-
-class CreditCard extends StatelessWidget {
-  final Color bgColor;
+class CreditCard extends StatefulWidget {
+  final int index;
   final bool isExpanded;
 
-  const CreditCard({
-    super.key,
-    required this.bgColor,
-    required this.isExpanded,
-  });
+  const CreditCard({super.key, required this.index, required this.isExpanded});
+
+  @override
+  State<CreditCard> createState() => _CreditCardState();
+}
+
+class _CreditCardState extends State<CreditCard> {
+  void _onTap() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => CardDetailScreen(index: widget.index),
+        fullscreenDialog: true,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return AbsorbPointer(
-      absorbing: !isExpanded, // true: ignore
-      child: GestureDetector(
-        onTap: _onTap,
-        child: Container(
-          width: double.infinity,
-          margin: EdgeInsets.only(bottom: 20),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            color: bgColor,
-          ),
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-            child: Column(
-              children: [
-                SizedBox(height: 100),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Credit Cards",
-                          style: TextStyle(color: Colors.white, fontSize: 16),
-                        ),
-                        Text(
-                          "**** **** **80",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                    Stack(
-                      clipBehavior: Clip.none,
-                      children: [
-                        Positioned(
-                          child: Container(
-                            width: 50,
-                            height: 50,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.red,
+    return Material(
+      type: MaterialType.transparency,
+      child: AbsorbPointer(
+        absorbing: !widget.isExpanded, // true: ignore
+        child: GestureDetector(
+          onTap: _onTap,
+          child: Container(
+            width: double.infinity,
+            margin: EdgeInsets.only(bottom: 20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: colors[widget.index],
+            ),
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+              child: Column(
+                children: [
+                  SizedBox(height: 100),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Credit Cards",
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                          Text(
+                            "**** **** **80",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      Stack(
+                        clipBehavior: Clip.none,
+                        children: [
+                          Positioned(
+                            child: Container(
+                              width: 50,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.red,
+                              ),
                             ),
                           ),
-                        ),
-                        Positioned(
-                          right: 20,
-                          child: Container(
-                            width: 50,
-                            height: 50,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.amber,
+                          Positioned(
+                            right: 20,
+                            child: Container(
+                              width: 50,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.amber,
+                              ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class CardDetailScreen extends StatelessWidget {
+  final int index;
+  const CardDetailScreen({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Transactions")),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Hero(
+              tag: "$index",
+              child: CreditCard(
+                index: index,
+                isExpanded: false, // ignore gesturedetector
+              ),
+            ),
+          ],
         ),
       ),
     );
